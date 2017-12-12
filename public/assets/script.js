453a55d24c9a63dbfbe577985234950211681eeb
4c <4c.mura@gmail.com>
2017-12-12 20:00:49 +0900

    init

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,184 +1,337 @@
-'use strict';
+"use strict";
 
-var scene = new THREE.Scene();
-var plane = void 0;
-var plane_god = void 0;
-var controls = void 0;
-var geometry = void 0;
-var geometry_god = void 0;
-var renderer = void 0;
-var camera = void 0;
-var model = [];
-var SEGX = 30;
-var SEGY = 30;
-var startTime = new Date();
-var rotate_speed = 0;
+/*
+「四角形オブジェクト100×100_ガウシアン_枠_時間発展_.html」をベースとして
+*/
+////////////////////////////////////////////////////////////////////
+// 物理系の定義
+////////////////////////////////////////////////////////////////////
+//２次元正方格子のサイズ
+var N = 100;
+var l = 1.0;
 
-//let model = {};
+var dt = 0.1; //時間間隔
+var dd = 1.0; //空間間隔
+var v = 4; //速度
 
+//境界条件の設定
+//var BC = "Neumann"; //or "Dirichlet"
+var BC = "Dirichlet";
 
-var vm = new Vue({
-  el: '#mycounter',
-  data: {
-    count: 0
-  },
-  methods: {
-    countUp: function countUp() {
-      this.count++;
-      changeRotateSpeed();
-    }
-  }
+var peakPosition = { x: 0, y: 0, z: 100, sigma2: 30 };
+var peakPosition_bound = { x_min: -50, x_max: 50, y_min: -50, y_max: 50, z_min: -50, z_max: 100, sigma2_min: 10, sigma2_max: 100 };
+
+//一時停止フラグ
+var restartFlag = false; //再計算フラグ
+var stopFlag = false; //一時停止フラグ
+
+var Tn = 3;
+var f = new Array(Tn);
+
+function initialCondition(parameter) {
+	var x0 = parameter.x;
+	var y0 = parameter.y;
+	var z0 = parameter.z;
+	var sigma2 = parameter.sigma2;
+	for (var t = 0; t < Tn; t++) {
+		f[t] = new Array(N);
+		for (i = 0; i <= N; i++) {
+			f[t][i] = new Array(N);
+			for (j = 0; j <= N; j++) {
+				var x = (-N / 2 + i) * l;
+				var y = (-N / 2 + j) * l;
+				//初期条件を与える
+				var z = z0 * Math.exp(-(Math.pow(x - x0, 2) + Math.pow(y - y0, 2)) / (2 * sigma2));
+				f[0][i][j] = z;
+			}
+		}
+	}
+	for (var i = 1; i <= N - 1; i++) {
+		for (var j = 1; j <= N - 1; j++) {
+			f[1][i][j] = f[0][i][j] + v * v / 2.0 * dt * dt / (dd * dd) * (f[0][i + 1][j] + f[0][i - 1][j] + f[0][i][j + 1] + f[0][i][j - 1] - 4.0 * f[0][i][j]);
+		}
+	}
+	if (BC == "Dirichlet") {
+		//ディリクレ境界条件
+		for (var i = 0; i <= N; i++) {
+			f[1][i][0] = f[1][i][N] = f[1][0][i] = f[1][N][i] = 0.0; //境界条件
+		}
+	} else if (BC == "Neumann") {
+		//ノイマン境界条件
+		for (var i = 1; i <= N - 1; i++) {
+			f[1][i][0] = f[1][i][1];
+			f[1][i][N] = f[1][i][N - 1];
+			f[1][0][i] = f[1][1][i];
+			f[1][N][i] = f[1][N - 1][i];
+		}
+		//角の処理
+		f[1][0][0] = (f[1][0][1] + f[1][1][0]) / 2;
+		f[1][0][N] = (f[1][0][N - 1] + f[1][1][N]) / 2;
+		f[1][N][0] = (f[1][N - 1][0] + f[1][N][1]) / 2;
+		f[1][N][N] = (f[1][N - 1][N] + f[1][N][N - 1]) / 2;
+	}
+}
+
+////////////////////////////////////////////////////////////////////
+// windowイベントの定義
+////////////////////////////////////////////////////////////////////
+window.addEventListener("load", function () {
+	resizeTo(516, 538);
+	initialCondition(peakPosition);
+	threeStart(); //Three.jsのスタート関数の実行
 });
 
-var vm_stop = new Vue({
-  el: '#mystop',
-  methods: {
-    hsStop: function hsStop() {
-      Speed_0();
-    }
-  }
-});
+////////////////////////////////////////////////////////////////////
+// Three.jsスタート関数の定義
+////////////////////////////////////////////////////////////////////
+function threeStart() {
+	initThree(); //Three.js初期化関数の実行
+	initCamera(); //カメラ初期化関数の実行
+	initLight(); //光源初期化関数の実行
+	initObject(); //オブジェクト初期化関数の実行
+	loop(); //無限ループ関数の実行
+}
+////////////////////////////////////////////////////////////////////
+// Three.js初期化関数の定義
+////////////////////////////////////////////////////////////////////
+//グローバル変数の宣言
+var renderer, //レンダラーオブジェクト
+scene, //シーンオブジェクト
+canvasFrame; //キャンバスフレームのDOM要素
+function initThree() {
+	//キャンバスフレームDOM要素の取得
+	canvasFrame = document.getElementById('canvas-frame');
+	//レンダラーオブジェクトの生成
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+	if (!renderer) alert('Three.js の初期化に失敗しました');
+	//レンダラーのサイズの設定
+	renderer.setSize(canvasFrame.clientWidth, canvasFrame.clientHeight);
+	//キャンバスフレームDOM要素にcanvas要素を追加
+	canvasFrame.appendChild(renderer.domElement);
 
-function renderFlag() {
-  'use strict';
+	//レンダラークリアーカラーの設定
+	//renderer.setClearColorHex(0x000000, 1.0);
 
-  var light = void 0;
-  var ambient = void 0;
-  var width = 1000;
-  var height = 750;
-  var modelPath = void 0;
+	//シーンオブジェクトの生成
+	scene = new THREE.Scene();
+}
+////////////////////////////////////////////////////////////////////
+// カメラ初期化関数の定義
+////////////////////////////////////////////////////////////////////
+//グローバル変数の宣言
+var camera; //カメラオブジェクト
+function initCamera() {
+	//カメラオブジェクトの生成
+	camera = new THREE.PerspectiveCamera(45, canvasFrame.clientWidth / canvasFrame.clientHeight, 1, 10000);
+	//カメラの位置の設定
+	camera.position.set(160, 0, 200);
+	//カメラの上ベクトルの設定
+	camera.up.set(0, 0, 1);
+	//カメラの中心位置ベクトルの設定
+	camera.lookAt({ x: 0, y: 0, z: 0 }); //トラックボール利用時は自動的に無効
 
-  //light
-  //light = new THREE.PointLight(0xffffff,2);
-  //light.position.set(100,0,300);
-  //scene.add(light);
+	//トラックボールオブジェクトの宣言
+	//trackball = new THREE.TrackballControls(camera, canvasFrame);
 
-  light = new THREE.DirectionalLight(0xffffff, 1);
-  light.position.set(100, 0, 300);
-  scene.add(light);
-  /*
-    ambient = new THREE.AmbientLight(0x404040);
-    scene.add(ambient);
-  */
-  //camera
-  //camera = new THREE.PerspectiveCamera(45, width /　height, 1 , 1000);
-  camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 4000);
-  camera.position.set(0, 0, 1000);
-  camera.lookAt(new THREE.Vector3(0, 0, 0));
+	//トラックボール動作範囲のサイズとオフセットの設定
+	// trackball.screen.width = canvasFrame.clientWidth;                        //横幅
+	// trackball.screen.height = canvasFrame.clientHeight;                      //縦幅
+	// trackball.screen.offsetLeft = canvasFrame.getBoundingClientRect().left;  //左オフセット
+	// trackball.screen.offsetTop = canvasFrame.getBoundingClientRect().top;    //右オフセット
 
-  //  camera.position.set(0, 400, 300);
-  //  camera.lookAt(scene.position);
+	//トラックボールの回転無効化と回転速度の設定
+	// trackball.noRotate = false;
+	// trackball.rotateSpeed = 2.0;
+	//
+	// //トラックボールの拡大無効化と拡大速度の設定
+	// trackball.noZoom = false;
+	// trackball.zoomSpeed = 1.0;
+	//
+	// //トラックボールのカメラ中心移動の無効化と中心速度の設定
+	// trackball.noPan = false;
+	// trackball.panSpeed = 1.0;
+	// trackball.target = new THREE.Vector3(0, 0, 10);
+	//
+	// //トラックボールのスタティックムーブの有効化
+	// trackball.staticMoving = true;
+	// //トラックボールのダイナミックムーブ時の減衰定数
+	// trackball.dynamicDampingFactor = 0.3;
+}
+////////////////////////////////////////////////////////////////////
+// 光源初期化関数の定義
+////////////////////////////////////////////////////////////////////
+//グローバル変数の宣言
+var directionalLight, //平行光源オブジェクト
+ambientLight; //環境光オブジェクト
+function initLight() {
+	//平行光源オブジェクトの生成
+	directionalLight = new THREE.DirectionalLight(0xDDDDDD, 1.0, 0);
+	//平行光源オブジェクトの位置の設定
+	directionalLight.position.set(30, 30, 100);
 
-  // helper 現在は非表示
-  var gridHelper = new THREE.GridHelper(200, 50);
-  scene.add(gridHelper);
-  var axisHelper = new THREE.AxisHelper(1000);
-  scene.add(axisHelper);
-  var lightHelper = new THREE.DirectionalLightHelper(light, 20);
-  scene.add(lightHelper);
+	//環境光オブジェクトの生成
+	ambientLight = new THREE.AmbientLight(0x222222);
+	//環境光オブジェクトのシーンへの追加
+	scene.add(ambientLight);
 
-  //controls
-  controls = new THREE.OrbitControls(camera);
-  //cameraの自動回転
-  controls.autoRotate = true;
-  controls.autoRotateSpeed = 1.5;
+	//平行光源オブジェクトのシーンへの追加
+	scene.add(directionalLight);
+}
+////////////////////////////////////////////////////////////////////
+// オブジェクト初期化関数の定義
+////////////////////////////////////////////////////////////////////
+//グローバル変数の宣言
+var axis,
+    //軸オブジェクト
+lattice,
+    //２次元格子オブジェクト
+cubes = []; //立方体オブジェクト
+function initObject() {
+	/*
+ 		//軸オブジェクトの生成
+ 		axis = new THREE.AxisHelper(100);
+ 		//軸オブジェクトのシーンへの追加
+ 		scene.add(axis);
+ 		//軸オブジェクトの位置座標を設定
+ 		axis.position.set(0, 0, 0);
+ */
+	//初期条件
+	initialCondition(peakPosition);
 
-  // renderer
-  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-  renderer.setSize(width, height);
-  renderer.setClearColor(0xffffff);
-  renderer.setPixelRatio(window.devicePixelRatio);
-  document.getElementById('stage').appendChild(renderer.domElement);
+	//形状オブジェクトの宣言と生成
+	var geometry = new THREE.Geometry();
+	//一片の長さ
+	for (i = 0; i <= N; i++) {
+		for (var j = 0; j <= N; j++) {
+			var x = (-N / 2 + i) * l;
+			var y = (-N / 2 + j) * l;
+			//初期条件を与える
+			var z = f[0][i][j];
+			//頂点座標データの追加
+			geometry.vertices.push(new THREE.Vector3(x, y, z));
+		}
+	}
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			var ii = (N + 1) * i + j;
+			//面指定用頂点インデックスを追加
+			geometry.faces.push(new THREE.Face3(ii, ii + (N + 1), ii + (N + 1) + 1, ii + 1));
+		}
+	}
+	//面の法線ベクトルを計算
+	geometry.computeFaceNormals();
+	//面の法線ベクトルから頂点法線ベクトルの計算
+	geometry.computeVertexNormals();
 
-  //var texture = new THREE.TextureLoader().load("./public/img/italy.jpg");
-  //var texture = new THREE.TextureLoader().load("./public/img/dice.jpg");
-  //テクスチャ読み込み
-  var loader = new THREE.TextureLoader();
-  //let texture=loader.load('./public/img/dice.jpg');
-  // loader.load('./public/img/yoko.jpg', (texture) => {
-  loader.load('./public/img/yoko.jpg', function (texture) {
-    geometry = new THREE.PlaneGeometry(973, 703, SEGX, SEGY);
-    //let geometry = new THREE.PlaneGeometry( 5, 20, 32 );
+	//材質オブジェクトの宣言と生成
+	var loader = new THREE.TextureLoader();
+	var texture = loader.load('./public/img/italy.jpg');
 
-    texture.minFilter = THREE.LinearFilter;
-    var material = new THREE.MeshBasicMaterial({ map: texture, transparent: true });
-    //let material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-    //var material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-    //var material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-    plane = new THREE.Mesh(geometry, material);
-    var helper = new THREE.VertexNormalsHelper(plane, 2, 0xee0000, 1);
-    scene.add(helper);
-    // let helper_wier = new THREE.WireframeGeometry( plane, 20, 0xee0000, 1 );
-    // scene.add(helper_wier);
+	var material = new THREE.MeshBasicMaterial({ color: 0xafeeee, side: THREE.DoubleSide, specular: 0xffffff, shininess: 250 });
+	//var material = new THREE.MeshBasicMaterial({ map: texture,  side: THREE.DoubleSide});
+	texture.minFilter = THREE.LinearFilter;
+	//立方体オブジェクトの生成
+	lattice = new THREE.Mesh(geometry, material);
 
-    var wireframe = new THREE.WireframeGeometry(geometry);
-    var line = new THREE.LineSegments(wireframe);
-    line.material.depthTest = false;
-    line.material.opacity = 1;
-    line.material.transparent = true;
-    scene.add(new THREE.BoxHelper(line));
-    //line.material.transparent = true;
-    scene.add(line);
+	//形状オブジェクトの宣言と生成
+	var geometry = new THREE.CubeGeometry(1, 101, 30);
+	//材質オブジェクトの宣言と生成F
+	var material = new THREE.MeshPhongMaterial({ color: 0xFFFFFF, specular: 0xffffff, shininess: 100, transparent: true, opacity: 0.2 });
 
-    var fnh = new THREE.FaceNormalsHelper(plane, 5);
-    scene.add(fnh);
-
-    scene.add(plane);
-
-    var loader2 = new THREE.TextureLoader();
-    loader.load('./public/img/flag_moyo.png', function (texture) {
-      geometry_god = new THREE.PlaneGeometry(973, 703, SEGX, SEGY);
-      var material_god = new THREE.MeshBasicMaterial({ map: texture, transparent: true });
-      //let material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-      //var material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-      //var material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-      plane_god = new THREE.Mesh(geometry_god, material_god);
-      plane_god.scale.set(0.7, 0.7, 0.7);
-
-      //scene.add(plane_god);
-      render();
-    });
-  });
-  //console.log(texture);
-  //texture.needsUpdate = true;
-  //var geometry = new THREE.PlaneGeometry(1300, 1228, SEGX, SEGY);
+	for (var i = 0; i < 4; i++) {
+		//立方体オブジェクトの生成
+		cubes[i] = new THREE.Mesh(geometry, material);
+		//立方体オブジェクトのシーンへの追加
+		scene.add(cubes[i]);
+	}
+	//立方体オブジェクトの位置座標を設定
+	cubes[0].position.set(50, 0, 15);
+	cubes[1].position.set(-50, 0, 15);
+	cubes[2].position.set(0, 50, 15);
+	cubes[2].rotation.set(0, 0, Math.PI / 2);
+	cubes[3].position.set(0, -50, 15);
+	cubes[3].rotation.set(0, 0, Math.PI / 2);
 }
 
-function render() {
+////////////////////////////////////////////////////////////////////
+// 無限ループ関数の定義
+////////////////////////////////////////////////////////////////////
+//グローバル変数の宣言
+var step = 0; //ステップ数
+function loop() {
+	//トラックボールによるカメラオブジェクトのプロパティの更新
+	//trackball.update();
 
-  plane.geometry.verticesNeedUpdate = true; //これを忘れずに書く
-  plane_god.geometry.verticesNeedUpdate = true; //これを忘れずに書く
-  var time = (new Date() - startTime) / 1000;
+	//時刻の取得
+	var time = step * dt;
+	//時間発展
+	if (stopFlag == false) {
+		//ステップ数のインクリメント
+		step++;
+		time = step * dt;
+		for (var i = 1; i <= N - 1; i++) {
+			for (var j = 1; j <= N - 1; j++) {
+				f[2][i][j] = 2.0 * f[1][i][j] - f[0][i][j] + v * v * dt * dt / (dd * dd) * (f[1][i + 1][j] + f[1][i - 1][j] + f[1][i][j + 1] + f[1][i][j - 1] - 4.0 * f[1][i][j]);
+			}
+		}
+		if (BC == "Dirichlet") {
+			//ディリクレ境界条件
+			for (var i = 0; i <= N; i++) {
+				f[2][i][0] = f[2][i][N] = f[2][0][i] = f[2][N][i] = 0.0; //境界条件
+			}
+		} else if (BC == "Neumann") {
+			//ノイマン境界条件
+			for (var i = 1; i <= N - 1; i++) {
+				f[2][i][0] = f[2][i][1];
+				f[2][i][N] = f[2][i][N - 1];
+				f[2][0][i] = f[2][1][i];
+				f[2][N][i] = f[2][N - 1][i];
+			}
+			//角の処理
+			f[2][0][0] = (f[2][0][1] + f[2][1][0]) / 2;
+			f[2][0][N] = (f[2][0][N - 1] + f[2][1][N]) / 2;
+			f[2][N][0] = (f[2][N - 1][0] + f[2][N][1]) / 2;
+			f[2][N][N] = (f[2][N - 1][N] + f[2][N][N - 1]) / 2;
+		}
+		//次の計算のために配列の数値を入れかえる。ここで過去の情報は失われる。
+		for (var i = 0; i <= N; i++) {
+			for (var j = 0; j <= N; j++) {
+				f[0][i][j] = f[1][i][j];
+				f[1][i][j] = f[2][i][j];
+			}
+		}
+	}
 
-  for (var i = 0; i < SEGX + 1; i++) {
-    for (var j = 0; j < SEGY + 1; j++) {
-      //(i,j)のvertexを得る
-      var index = j * (SEGX + 1) + i % (SEGX + 1);
-      var vertex = plane.geometry.vertices[index];
-      // //			let vertex_god = plane_god.geometry.vertices[index];
-      //時間経過と頂点の位置によって波を作る
-      var amp = 100; //振幅
-      //vertex.z = amp * Math.sin( -i/2 + time*10 );
-      // vertex_god.z = amp * Math.sin( -i/2 + time*10) + 200;
-    }
-  }
+	var a = 0;
+	for (i = 0; i <= N; i++) {
+		for (j = 0; j <= N; j++) {
+			var x = (-N / 2 + i) * l;
+			var y = (-N / 2 + j) * l;
+			var z = f[1][i][j];
+			//頂点座標データの追加
+			lattice.geometry.vertices[a].z = z;
+			a++;
+		}
+	}
 
-  plane_god.rotation.z += rotate_speed;
-  requestAnimationFrame(render);
-  //controls.update();
-  renderer.render(scene, camera);
-}
+	lattice.geometry.normalsNeedUpdate = true;
+	lattice.geometry.verticesNeedUpdate = true;
+	//面の法線ベクトルを計算
+	lattice.geometry.computeFaceNormals();
+	//面の法線ベクトルから頂点法線ベクトルの計算
+	lattice.geometry.computeVertexNormals();
 
-renderFlag();
+	//立方体オブジェクトのシーンへの追加
+	scene.add(lattice);
 
-function changeRotateSpeed() {
-  //controls.autoRotateSpeed = vm.count*10;
-  rotate_speed -= vm.count * 0.01;
-}
+	//クリアーカラーで初期化
+	renderer.clear();
+	//レンダリング
+	renderer.render(scene, camera);
 
-function Speed_0() {
-  vm.count = 0;
-  rotate_speed = 0;
-  //addSpinner();
+	scene.remove(lattice);
+
+	//「loop()」関数の呼び出し
+	requestAnimationFrame(loop);
 }
 //# sourceMappingURL=script.js.map
